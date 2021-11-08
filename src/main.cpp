8998829fa2ae45681b6f92c150df985fdf6f4fe2
droidek123 <droidek123@interia.pl>
2021-11-08 17:35:46 +0100

    Add menu

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,224 @@
 
 using namespace std;
 
+void displayMainMenu();
+
+void menuBruteForce();
+
+void menuBnB();
+
+void menuDynamicPrograming();
+
 int main() {
-    Result result;
-    Graph graph,graph1;
+    bool quit = false;
+    char option;
+    do {
+        displayMainMenu();
+        cin >> option;
+
+        cout << endl;
+        switch (option) {
+            case '0':
+                quit = true;
+                break;
+            case '1':
+                menuBruteForce();
+                break;
+
+            case '2':
+                menuBnB();
+                break;
+
+            case '3':
+                menuDynamicPrograming();
+                break;
+            default:
+                cout << "Wrong option chosen.\n";
+
+        }
+
+    } while (!quit);
+
+    return 0;
+
+}
+
+
+void menuBruteForce(){
+    bool quit = false;
+    char option;
+    string name;
+    Graph graph;
+
+    do {
+        cout << "================= BRUTE FORCE =================" << endl;
+        cout << "\t   1. Load from file" << endl;
+        cout << "\t   2. Generate adjacency matrix" << endl;
+        cout << "\t   3. Display adjacency matrix" << endl;
+        cout << "\t   4. Execute algorithm without Algorithms" << endl;
+        cout << "\t   5. Execute algorithm with Algorithms" << endl;
+        cout << "\t   0. Back to the main menu" << endl<<endl;
+        cout << "Chosen option: ";
+        cin >> option;
+
+        cout << endl;
+        switch (option) {
+            case '0':
+                quit = true;
+                break;
+            case '1':
+                cout << ("Plese enter name of the file: ");
+                cin >> name;
+                graph.load_data(name);
+                break;
+            case '2':
+                int x;
+                cout << "Enter amount of nodes: ";
+                cin >> x;
+                //graph.generateGraph(x);
+                break;
+            case '3':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                graph.display();
+                break;
+            case '4':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                {
+                    graph.brute_force();
+                }
+                break;
+            default:
+                cout << "Wrong option chosen.\n";
+
+        }
+
+    } while (!quit);
+}
+
+void displayMainMenu(){
+    // system("clear");
+    cout << "================== MAIN MENU ==================" << endl;
+    cout << "\t   1. Brute Force" << endl;
+    cout << "\t   2. B&B" << endl;
+    cout << "\t   3. Dynamic programming" << endl;
+    cout << "\t   0. Exit" << endl<<endl;
+    cout << "Chosen option: ";
+}
+
+void menuDynamicPrograming(){
+    bool quit = false;
+    char option;
+    string name;
+    Graph graph;
     Dynamic_programming dynamicProgramming;
-    graph.load_data("tsp6_1");
+    do {
+        cout << "================= DYNAMIC PROGRAMING =================" << endl;
+        cout << "\t   1. Load from file" << endl;
+        cout << "\t   2. Generate adjacency matrix" << endl;
+        cout << "\t   3. Display adjacency matrix" << endl;
+        cout << "\t   4. Execute algorithm" << endl;
+        cout << "\t   0. Back to the main menu" << endl<<endl;
+        cout << "Chosen option: ";
+        cin >> option;
 
-    result = dynamicProgramming.solve(graph);
-    cout << "Najlepszy wynik" << endl;
-    cout << result.best_score << endl;
-    cout << "Najlepsza scieszka" << endl;
-    for (auto r: result.list_of_nodes){
-        cout << to_string(r) + " ";
-    }
-    cout << endl;
+        cout << endl;
+        switch (option) {
+            case '0':
+                quit = true;
+                break;
+            case '1':
+                cout << ("Plese enter name of the file: ");
+                cin >> name;
+                graph.load_data(name);
+                break;
+            case '2':
+                int x;
+                cout << "Enter amount of nodes: ";
+                cin >> x;
+                //graph.generateGraph(x);
+                break;
+            case '3':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                graph.display();
+                break;
+            case '4':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                {
+                    dynamicProgramming.solve(graph);
+                }
+                break;
+            default:
+                cout << "Wrong option chosen.\n";
 
-    graph1.load_data("tsp6_2");
+        }
 
-    result = dynamicProgramming.solve(graph1);
-    cout << "Najlepszy wynik" << endl;
-    cout << result.best_score << endl;
-    cout << "Najlepsza scieszka" << endl;
-    for (auto r: result.list_of_nodes){
-        cout << to_string(r) + " ";
-    }
+    } while (!quit);
+}
 
+void menuBnB(){
+    bool quit = false;
+    char option;
+    string name;
+    Graph graph;
+    //BranchAndBound dBnB;
+    do {
+        cout << "================= BRANCH AND BOUND =================" << endl;
+        cout << "\t   1. Load from file" << endl;
+        cout << "\t   2. Generate adjacency matrix" << endl;
+        cout << "\t   3. Display adjacency matrix" << endl;
+        cout << "\t   4. Execute algorithm" << endl;
+        cout << "\t   0. Back to the main menu" << endl<<endl;
+        cout << "Chosen option: ";
+        cin >> option;
+
+        cout << endl;
+        switch (option) {
+            case '0':
+                quit = true;
+                break;
+            case '1':
+                cout << ("Plese enter name of the file: ");
+                cin >> name;
+                graph.load_data(name);
+                break;
+            case '2':
+                int x;
+                cout << "Enter amount of nodes: ";
+                cin >> x;
+                //graph.generateGraph(x);
+                break;
+            case '3':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                graph.display();
+                break;
+            case '4':
+                if(graph.getNumberOfVertices() <=1){
+                    cout << ("No adjacency matrix was created.\n");
+                    break;
+                }
+                {
+                    //dBnB.bnb(graph);
+                }
+                break;
+            default:
+                cout << "Wrong option chosen.\n";
+
+        }
+
+    } while (!quit);
 }
